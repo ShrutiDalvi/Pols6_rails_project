917b5eda2fc7555085b7275d917b4723371d8c86
Ranjeet <shruti@weboniselab.com>
2012-12-20 16:17:59 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pols6RailsApp::Application.config.secret_token = '37c98930df6f9e1c81b499d641463ba91153fb0e932ae4a4a2106f7a18d83c6e331fe5c97b7c24ce528d8843eaaaca434d5c8ab8eadbb94990b020e2ce8af94c'
