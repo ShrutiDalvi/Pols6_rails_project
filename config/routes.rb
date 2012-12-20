a42bc7cf95cf8961540c0925017092157cda85be
Ranjeet <shruti@weboniselab.com>
2012-12-20 20:44:02 +0530

    Database created

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Pols6RailsApp::Application.routes.draw do
     get "users/new"
 
     root to: 'static_pages#home'
-
     match '/signup',  to: 'users#new'
 
     match '/help',    to: 'static_pages#help'
