b249cb79b4b34f0ed86a62014eca1525b6baec22
Ranjeet <shruti@weboniselab.com>
2012-12-21 17:21:51 +0530

    Everything working properly.....9th chapter completede

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    end
+  end
 
   def create
-      user = User.find_by_email(params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        sign_in user
-        redirect_to user
-      else
-        flash.now[:error] = 'Invalid email/password combination'
-        render 'new'
-      end
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to root_path
+      flash.now[:notice] = 'Signed Successfully'
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
     end
+  end
 
 
   def destroy
-      sign_out
-      redirect_to root_url
-    end
+    sign_out
+    redirect_to signin_url
   end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,51 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def show
-     @user = User.find(params[:id])
-   end
+    @user = User.find(params[:id])
+  end
+
   def new
-     @user = User.new
+    @user = User.new
   end
 
   def create
-      @user = User.new(params[:user])
-      if @user.save
-        sign_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
-      else
-        render 'new'
-      end
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
     end
- end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+end
 
